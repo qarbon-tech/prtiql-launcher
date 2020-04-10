5ea3ec86dcfed859361fe8d8db05c3a6af6c0c6c
Nuwan Jayawardene <nuwanjayawardene@gmail.com>
2020-04-11 05:02:31 +0530

    Add sidepanel with game info

--- renderer.js
+++ renderer.js
@@ -9,17 +9,32 @@ const Store = require('electron-store');
 const shell = require('shelljs');
 
 var leftBlock = document.getElementById("left-block");
+var leftDetailsBlock = document.getElementById("details-left-block");
 var instructionsRightBlock = document.getElementById("instructions-right-block");
 var listRightBlock = document.getElementById("list-right-block");
 const wave1 = document.getElementsByClassName("wave");
 const wave2 = document.getElementsByClassName("wave2");
 
 // Setup dependencies and install Partiql
-$("#intialize-btn").click(function(){
+$("#intialize-btn").click(function() {
   instructionsRightBlock.classList.add('animated', 'fadeOutUp');
   
   listRightBlock.style.visibility = "visible";
   listRightBlock.classList.add('animated', 'fadeInUp');
+});
+
+// Pulls the game details drawer from left side
+function pullGameDetailsDrawer(name, desc) {
+  if ($(leftBlock).css('display') == 'none') {
+    //console.log(name + " " + desc + " hidden");
+
+  } else {
+    //console.log("shown");
+  }
+}
+
+$("#journey").click(function() {
+  pullGameDetailsDrawer("Name","Test description")
 });
 
 function setGameDetails() {

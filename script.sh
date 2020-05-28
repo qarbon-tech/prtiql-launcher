42adc39aef344035edbfa9d436e29281b1eb8751
n-jay <nuwanjayawardene@gmail.com>
2020-05-28 13:19:56 +0530

    Update bash script to test callback

--- script.sh
+++ script.sh
@@ -1,2 +1,14 @@
-# echo "HELLO WORLD"
+echo "HELLO WORLD"
 ls -al
+
+input=3
+tic=`date +%S`
+elap_time=0
+
+while [ "$elap_time" -le "$input" ]; 
+do
+    toc=`date +%S`
+    elap_time=`expr $toc - $tic`
+done
+
+echo "TIME ELAPSED"
